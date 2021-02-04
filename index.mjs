66781f7df05d735896a926afa7aab646184a80eb
edeced <ed_ced@icloud.com>
2021-02-04 12:45:54 -0600

    initial commit to node sample

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import { info } from "./info.js"
+// console.log("Hello there")
+// console.log("Sample node app")
+
+const { bio } = info
+
+console.log(bio)
---
+++ info.mjs
@@ -0,0 +1,15 @@
+export const info = [{
+    bio: "I'm a Fullstack Developer, who enjoys practicing yoga, recreational sports and traveling",
+    }, {
+    experience: {
+        msimbo: "MSIMBO coding academy",
+        ulem: "ULEM Community Project",
+        personal: "Github @ edeced"
+        },
+    }, {
+    skills: {
+        frontend: "Javascript, HTML, css",
+        backend: "Node.js, npm and deployment",
+        },
+    },  
+]
