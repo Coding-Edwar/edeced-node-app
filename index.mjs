9f87efcac38580fff9d77b938ea6f2949f6e3643
edeced <ed_ced@icloud.com>
2021-02-08 07:49:12 -0600

    completes npm credentials packages

--- index.mjs
+++ index.mjs
@@ -1,8 +1,52 @@
+#!/usr/bin/env
+
 import { info } from "./info.mjs";
 
+const { basic: {bio: {fName,lName, email, summary}} } = info;
 
-// const { description: { name: {fName} }, } = info;
-// const { description: { bio: {profession} } } = info;
-// const { description: { profiles: {github} } } = info;
+const { basic: {profiles: { github, linkedin, twitter}} } = info;
 
-console.log(info.description.name.fName)     
+const { basic: { work: { profession, experience, highlights} } } = info;
+
+const { basic: { techStack: { programLang, frameworks, otherTools}}} = info;
+
+const {basic: {interest: {sports, art, relaxing}}} = info; 
+
+console.log(`
+Hello, thanks for viewing my npm app. My is ${fName}, ${lName}. 
+--------------------------------------------------- 
+
+    ${summary}
+
+----------------------------------------------------  
+If you have any comments, questions or concerns please email me at ${email}. Thank you,
+---------------------------------------------------- 
+    
+
+    You can visit my online profiles here:
+
+        - ${github}
+        - ${linkedin}
+        - ${twitter}
+
+
+    Career
+----------------------------------------------------
+        ${profession}
+
+Certified by:
+    ${experience}
+
+Highlights:
+    ${highlights}
+
+    Proficient Pgramming Skills:
+    ----------------------------------------------------
+        Languages: ${programLang}
+        Frameworks: ${frameworks}
+
+----------------------------------------------------
+My Perosnal Interests:
+        Sports: ${sports}
+        Art: ${art}
+        Realaxing: ${relaxing}`)
--- info.mjs
+++ info.mjs
@@ -1,25 +1,42 @@
 // console.log("hello there")
 
 export const info = {
-    description: {
-        name: {
-            fName: "Edward",
-            lName: "C"
-        },
+    basic: {
         bio: {
-            profession: "Fullstack Developer",
-            experience: "ULEM MSIMBO Coding Academy",
-            hobbies: {
-                physical: "Yoga, Basketball, Volleyball",
-                art: "panting, music",
-                mental: "Meditation, Gardening"
+            fName: "Edward",
+            lName: "C",
+            email: "cedanoedward@gmail.com",
+            summary: "I'm a Fullstack Developer, certified by the MISMBO Coding Academy at ULEM. The web-apps I've built demonstrate my understanding of frontend design and backend funtionality. I am a yogi that happens to love code.",
+            location: {
+                city: "Chicago",
+                countryCode: "US",
+                region: "Midwest"
             },
         },
         profiles: {
             github: "github.com/edeced",
             linkedin: "linkedin.com/in/edward-cedano-592922a7/",
-            twitter: "twitter.com/ededced"
-        }
+            twitter: "twitter.com/ededced",
+        },
+        work: {
+            profession: "Fullstack Developer",
+            experience: "ULEM MSIMBO Coding Academy",
+            highlights: " Recieved certificate of completion for 6 months of intense computer programming training. Built usebale apps for jobtracking and personalized meal-planning. Contributed to Wordpress community project.",
+            },
+            techStack: {
+                programLang:"Javascript, CSS & HTML",
+                frameworks: "React.js, Vue.js, Alpine.js, SVELTE, tailwindcss, Node.js",
+                otherTools: "yarn, npm, github, postman, fauna databeses, ngrok,",  
+            },
+            volunteer:{
+                organization: "ULEM",
+                website: "https://www.ulem.org/"
+            },
+            interest: {
+                sports: "Yoga, Basketball, Volleyball",
+                art: "painting, music",
+                relaxing: "Yoga, Meditation, Gardening"
+            },
     },
 }
 
