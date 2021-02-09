227efd3f391bb53820a336fff6263440ba18f07e
edeced <ed_ced@icloud.com>
2021-02-09 15:07:50 -0600

    adds home app and link to info

--- server.mjs
+++ server.mjs
@@ -1,10 +1,18 @@
 import express from 'express';
 import { info } from './info.mjs';
+import { home } from './home.mjs';
 
 
 const PORT = 5000;
 const app = express();
 
+// route & callback
+app.get ('/', (req, res) => res.send(home) );
+app.get ('/id', (req, res) => {
+    res.send({
+        id: Math.random()
+    })
+});
 app.get('/edeced', (req, res) => res.send(info) );
 
 app.listen('5000', () => {
