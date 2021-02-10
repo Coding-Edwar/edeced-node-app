cf1edd46981125947d62c53a96f39ba433e4a148
edeced <ed_ced@icloud.com>
2021-02-10 03:22:15 -0600

    style npm package

--- index.mjs
+++ index.mjs
@@ -2,51 +2,101 @@
 
 import { info } from "./info.mjs";
 
-const { basic: {bio: {fName,lName, email, summary}} } = info;
+const { basic: {bio: {fName,lName, email, summary } } } = info;
 
-const { basic: {profiles: { github, linkedin, twitter}} } = info;
+const { basic: { profiles: [ { network, username, profileURL }, { network2, username2, profileURL2 }, {network3, username3, profileURL3 } ] } } = info;
 
-const { basic: { work: { profession, experience, highlights} } } = info;
+const { basic: { work: [ { company, position, website, highlights: [ { project, contribution } ] }, { company2, position2, website2, highlights2: [ { project2, contribution2 } ] } ] } } = info;
 
-const { basic: { techStack: { programLang, frameworks, otherTools}}} = info;
+const { basic: { skills: [ { skill, exampleURL, tools }, { skill2, exampleURL2, tools2 } ] } } = info;
 
-const {basic: {interest: {sports, art, relaxing}}} = info; 
+const { basic: { techStack: { programmingLanguages: [ { js, jsFrameworks }, { css, cssFrameworks }, { html } ], } } } = info;
+
+const { basic: { languages: [ { languageS, fluency }, { languageE } ] } } = info;
+
+const { basic: { references: [ { ref1, reference1 }, { ref2, reference2 } ] } } = info;
+
+const { basic: { interest: [ { title1 }, { title2 }, { title3 }, { title4 } ] } } = info;
+
 
 console.log(`
-Hello, thanks for viewing my npm app. My name is ${fName}, ${lName}. 
---------------------------------------------------- 
+Hello, thanks for viewing my npm package. My name is ${fName} ${lName}. 
 
-    ${summary}
+${summary}
 
-----------------------------------------------------  
-If you have any comments, questions or concerns please email me at ${email}. Thank you,
----------------------------------------------------- 
-    
-
-    You can visit my online profiles here:
-
-        - ${github}
-        - ${linkedin}
-        - ${twitter}
+  
+If you have any questions email me at ${email}.`)
 
 
-    Career
-----------------------------------------------------
-        ${profession}
+console.log(`
+OR 
 
-Certified by:
-    ${experience}
+Visit my online profiles:                                 
+-------------------------------------------------------------
+| Network       | Username                  | Link          |
+-------------------------------------------------------------
+* ${network}        | ${username}                    | ${profileURL}
+-------------------------------------------------------------
+* ${network2}      | ${username2}    | ${profileURL2}
+-------------------------------------------------------------
+* ${network3}       | ${username3}                   | ${profileURL3}
+-------------------------------------------------------------
+`);
 
-Highlights:
-    ${highlights}
+console.log(`
+Jobs:
 
-    Proficient Pgramming Skills:
-    ----------------------------------------------------
-        Languages: ${programLang}
-        Frameworks: ${frameworks}
+- Position: ${position}
+- Company: ${company}
+- Website: ${website}
+    Highligts: ${contribution} for ${project}
 
-----------------------------------------------------
-My Perosnal Interests:
-        Sports: ${sports}
-        Art: ${art}
-        Realaxing: ${relaxing}`)
+- Position: ${position2}
+- Company: ${company2}
+- Website: ${website2}
+    Highligts: ${contribution2} for ${project2}
+
+Skills:
+- ${skill}
+    Tools:  ${tools}
+            ${exampleURL}
+
+- ${skill2}
+   Tools:   ${tools2}
+            ${exampleURL2}
+
+`);
+
+console.log(`
+
+-------------------------------------------------------------
+| Programming Languages & Frameworks                        |
+-------------------------------------------------------------
+* ${js} | ${jsFrameworks}                                   |
+-------------------------------------------------------------
+* ${css} | ${cssFrameworks}                                 |
+-------------------------------------------------------------
+* ${html} |                                                 |
+-------------------------------------------------------------
+* ${otherTools}
+`);
+
+console.log(`
+Bilingual speaker 
+- ${fluency} ${languageS} fluency
+- ${fluency} ${languageE} fluency
+`);
+
+console.log(`
+References:
+* ${ref1} - ${reference1}
+* ${ref2} - ${reference2}
+`);
+
+console.log(`
+Interests:
+* ${title1}
+* ${title2}
+* ${title3}
+* ${title4}
+`);
--- info.mjs
+++ info.mjs
@@ -4,43 +4,140 @@ export const info = {
     basic: {
         bio: {
             fName: "Edward",
-            lName: "C",
+            lName: "Cedano",
+            label: "Programmer",
             email: "cedanoedward@gmail.com",
-            summary: "I'm a Fullstack Developer, certified by the MISMBO Coding Academy at ULEM. The web-apps I've built demonstrate my understanding of frontend design and backend funtionality. I am a yogi that happens to love code.",
+            phone: "(###) ###-###",
+            summary: "I am a yogi that happens to love code. As a Fullstack Developer, the apps I've built demonstrate my understanding of frontend design and backend funtionality.",
             location: {
                 city: "Chicago",
                 countryCode: "US",
                 region: "Midwest"
             },
         },
-        profiles: {
-            github: "github.com/edeced",
-            linkedin: "linkedin.com/in/edward-cedano-592922a7/",
-            twitter: "twitter.com/ededced",
-        },
-        work: {
-            profession: "Fullstack Developer",
-            experience: "ULEM MSIMBO Coding Academy",
-            highlights: " Recieved certificate of completion for 6 months of intense computer programming training. Built usebale apps for jobtracking and personalized meal-planning. Contributed to Wordpress community project.",
+        profiles: [
+            {
+            network: "Github",
+            username: "edeced",
+            profileURL: "https://github.com/edeced"
             },
-            techStack: {
-                programLang:"Javascript, CSS & HTML",
-                frameworks: "React.js, Vue.js, Alpine.js, SVELTE, tailwindcss, Node.js",
-                otherTools: "yarn, npm, github, postman, fauna databeses, ngrok,",  
+            {
+            network2: "Linkedin",
+            username2: "edward-cedano-592922a7",
+            profileURL2: "https://www.linkedin.com/in/edward-cedano-592922a7/"
             },
-            volunteer:{
-                organization: "ULEM",
-                website: "https://www.ulem.org/"
+            {
+            network3: "twitter",
+            username3: "ededced",
+            profileURL3: "https://twitter.com/ededced"
+            }
+        ],
+        work: [
+            {
+            company: "ULEM",
+            position: "Programmer",
+            website: "https://www.ulem.org/",
+            startDate: "2020-10-26",
+            endDate: "2021-04-01",
+            summary1: "Description...",
+            highlights: [ 
+                    {
+                    project: "Community Project",
+                    contribution: "Build section...",
+                    },
+                ]
             },
-            interest: {
-                sports: "Yoga, Basketball, Volleyball",
-                art: "painting, music",
-                relaxing: "Yoga, Meditation, Gardening"
+            {
+            company2: "Brodney & Sons Flooring",
+            position2: "Project Manager",
+            website2: "https://www.brodneysons.com/",
+            startDate2: "2018-07",
+            endDate2: "2020-09",
+            summary2: "Description...",
+            highlights2: [ 
+                    {
+                    project2: "MIT",
+                    contribution2: "Manage account...",
+                    },
+                ]
+            }
+        ],
+        skills: [
+            {
+            skill: "Web Development",
+            level: "Experienced",
+            exampleURL: "https://potential-jobs.edeced.vercel.app/",
+            tools: [
+              "HTML",
+              "CSS",
+              "Javascript"
+            ]
+          },
+          {
+            skill2: "Backend App Development",
+            level: "Intermediate",
+            exampleURL2: "https://arcane-woodland-74817.herokuapp.com/",
+            tools2: [
+              "Javascript",
+              "Node.js",
+              "npm",
+              "API's"
+            ]
+          }
+        ],   
+        techStack: {
+            programmingLangauges: [
+            {
+            js: "Javascript",
+            jsFrameworks: "React.js, Vue.js, Alpine.js, SVELTE, Node.js"
             },
+            { 
+            css: "CSS",
+            cssFrameworks: "Tailwindcss"
+            },
+            {
+            html: "HTML",
+            }
+        ],
+            otherTools: "yarn, npm, github, postman, fauna databeses, ngrok,", 
+    },
+        languages: [
+            {
+            languageS: "Spanish",
+            fluency: "Native"
+            },
+            {
+            languageE: "English",
+            fluency: "Native"
+            },
+        ],
+        references: [
+            {
+            ref1: "Joan Ortiz",
+            reference1: "https://twitter.com/ORTIZtheGREAT"
+            },
+            {
+            ref: "Christopher Cruz",
+            reference: "linkedin.com/in/chris-cruz-917a643b"
+            }
+        ],
+        interest: [
+            {
+            title: "Yoga",
+            keywords: "Yogi, breathing, stretch"
+            },
+            {
+            title2: "Basketball",
+            keywords: "Recreational, Running, Ball, Dribble"
+            },
+            {
+            title3: "Painting",
+            keywords: "Canvas, Paint, Art"
+            },
+            {
+            title4: "Music",
+            keywords: "Listening, Playing, Instrument"
+            },
+        ],
     },
 }
-
-
-// const { description: {bio, experience}} = info
-
-// console.log(`I'm a ${bio}, trained by ${experience}`)
