72f0e23393ee48d125ff1db3c20f455c1dcb1b7e
edeced <ed_ced@icloud.com>
2021-02-10 12:50:52 -0600

    updates greeting & message

--- home.mjs
+++ home.mjs
@@ -1,6 +1,7 @@
 export const home = {
   home: {
-    hello: "Hello Welcome to my resume as Node.js app.",
+    greeting: "Hello,", 
+    message: "Welome to my resume as a Node.js app.",
     },
     resume: "http://localhost:5000/edeced"
 }
