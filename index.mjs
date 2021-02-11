ba253ef842a4de7d9ecca1af351a3a49be4ae5bb
edeced <ed_ced@icloud.com>
2021-02-10 20:22:11 -0600

    updates

--- index.mjs
+++ index.mjs
@@ -2,21 +2,13 @@
 
 import { info } from "./info.mjs";
 
-
 const { basic: {bio: {fName,lName, email, summary } } } = info;
-
 const { basic: { profiles: [ { network, username, profileURL }, { network2, username2, profileURL2 }, {network3, username3, profileURL3 } ] } } = info;
-
 const { basic: { work: [ { company, position, website, highlights: [ { project, contribution } ] }, { company2, position2, website2, highlights2: [ { project2, contribution2 } ] } ] } } = info;
-
 const { basic: { skills: [ { skill, exampleURL, tools }, { skill2, exampleURL2, tools2 } ] } } = info;
-
 const { basic: { languages: [ { languageS, fluency }, { languageE } ] } } = info;
-
 const { basic: { references: [ { ref, reference }, { ref2, reference2 } ] } } = info;
-
 const { basic: { interest: [ { title, keywords }, { title2, keywords2 }, { title3, keywords3 }, { title4, keywords4 } ] } } = info;
-
 
 console.log(`
 Hello, thanks for viewing my npm package. My name is ${fName} ${lName}. 
